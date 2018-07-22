11834877f055f9c3e101c0054f4600c1ea8218e7
Nicolas Mémeint <nicolas.memeint@gmail.com>
2018-07-22 19:35:14 +0200

    Added PartialEq, IntoIterator for easier usage + Added test_sub & better test_add

--- src/matrix.rs
+++ src/matrix.rs
@@ -3,7 +3,7 @@ use std::ops::Add;
 
 #[derive(Debug, Clone)]
 pub struct Matrix<T> {
-	pub data: Vec<T>,
+	data: Vec<T>,
 	pub rows: usize,
 	pub cols: usize
 }
@@ -18,12 +18,12 @@ impl<T> Matrix<T> {
 				assert_eq!(data.len(), rows * cols);
 				data
 			},
-			rows: rows,
-			cols: cols
+			rows,
+			cols
 		}
 	}
 
-	pub fn get<'a>(&'a self, row: usize, col: usize) -> Option<&'a T> {
+	pub fn get(&self, row: usize, col: usize) -> Option<&T> {
 		if row < self.rows && col < self.cols {
 			Some(&self.data[row + col * self.rows])
 		} else {
@@ -31,11 +31,20 @@ impl<T> Matrix<T> {
 		}
 	}
 
-	pub fn get_mut<'a>(&'a mut self, row: usize, col: usize) -> Option<&'a mut T> {
+	pub fn get_mut(&mut self, row: usize, col: usize) -> Option<&mut T> {
 		if row < self.rows && col < self.cols {
 			Some(&mut self.data[row + col * self.rows])
 		} else {
 			None
+		}
+	}
+
+	pub fn set(&mut self, row: usize, col: usize, value: T) -> bool {
+		if row < self.rows && col < self.cols {
+			self.data[row + col * self.rows] = value;
+			true
+		} else {
+			false
 		}
 	}
 
@@ -60,6 +69,65 @@ impl<T> Matrix<T> {
 	}
 }
 
+// PartialEq implementation
+
+impl<T> PartialEq for Matrix<T> where T: PartialEq + ::std::fmt::Display {
+	fn eq(&self, rhs: &Self) -> bool {
+		self.rows == rhs.rows &&
+		self.cols == rhs.cols &&
+		self.data.iter().zip(rhs.data.iter())
+			.all(|(a, b)| *a == *b)
+	}
+}
+
+// IntoIterator implementation
+
+impl<T> IntoIterator for Matrix<T> {
+	type Item = T;
+	type IntoIter = ::std::vec::IntoIter<T>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		self.data.into_iter()
+	}
+}
+
+impl<'a, T> IntoIterator for &'a Matrix<T> {
+	type Item = &'a T;
+	type IntoIter = ::std::slice::Iter<'a, T>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		self.data.iter()
+	}
+}
+
+impl<'a, T> IntoIterator for &'a mut Matrix<T> {
+	type Item = &'a mut T;
+	type IntoIter = ::std::slice::IterMut<'a, T>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		self.data.iter_mut()
+	}
+}
+
+// Add implementation
+
+impl<T> Add for Matrix<T> where T: Add<Output=T> + Copy {
+	type Output = Matrix<T>;
+
+	fn add(mut self, other: Matrix<T>) -> Matrix<T> {
+		// Reuse self because memory for result
+
+		assert!(self.rows == other.rows);
+		assert!(self.cols == other.cols);
+
+		for (a, b) in (&mut self).into_iter().zip(other.into_iter()) {
+			*a = *a + b;
+		}
+
+		self
+	}
+}
+
 impl<'a, T> Add for &'a Matrix<T> where T: Add<Output=T> + Copy {
 	type Output = Matrix<T>;
 
@@ -75,6 +143,25 @@ impl<'a, T> Add for &'a Matrix<T> where T: Add<Output=T> + Copy {
 			rows: self.rows,
 			cols: self.cols
 		}
+	}
+}
+
+// Sub implementation
+
+impl<T> Sub for Matrix<T> where T: Sub<Output=T> + Copy {
+	type Output = Matrix<T>;
+
+	fn sub(mut self, other: Matrix<T>) -> Matrix<T> {
+		// Reuse self because memory for result
+
+		assert!(self.rows == other.rows);
+		assert!(self.cols == other.cols);
+
+		for (a, b) in (&mut self).into_iter().zip(other.into_iter()) {
+			*a = *a - b;
+		}
+
+		self
 	}
 }
 
--- src/tests.rs
+++ src/tests.rs
@@ -8,44 +8,67 @@ fn test_add() {
 	// Test square matrix
 	{
 		let inc = Matrix::new(3, 3, 0..9);
-
 		let dec = Matrix::new(3, 3, (0..9).map(|n| 8-n));
 
-		let res = &inc + &dec;
-		for i in 0..9 {
-			assert_eq!(res.data[i], 8);
+		let res_copy = &inc + &dec;
+		let res_move = inc + dec;
+		assert_eq!(res_copy, res_move);
+
+		for n in res_move {
+			assert_eq!(n, 8);
 		}
 	}
 	
 	// Test != rows
 	{
-		let mut small = Matrix::new(3, 3, 0..9);
-		for i in 0..9 {
-			small.data[i] = i;
-		}
+		let small = Matrix::new(3, 3, 0..9);
+		let large = Matrix::new(6, 3, 0..18);
 
-		let mut large = Matrix::new(6, 3, 0..18);
-		for i in 0..9 {
-			large.data[i] = 8-i;
-		}
-
-		let res = catch_unwind(|| &small + &large);
+		let res = catch_unwind(|| small + large);
 		assert!(res.is_err());
 	}
 
 	// Test != colums
 	{
-		let mut small = Matrix::new(3, 3, 0..9);
-		for i in 0..9 {
-			small.data[i] = i;
-		}
+		let small = Matrix::new(3, 3, 0..9);
+		let large = Matrix::new(3, 6, 0..18);
 
-		let mut large = Matrix::new(3, 6, 0..18);
-		for i in 0..9 {
-			large.data[i] = 8-i;
-		}
+		let res = catch_unwind(|| small + large);
+		assert!(res.is_err());
+	}
+}
 
-		let res = catch_unwind(|| &small + &large);
+#[test]
+fn test_sub() {
+	// Test square matrix
+	{
+		let inc = Matrix::new(3, 3, 0..9);
+		let dec = Matrix::new(3, 3, 0..9);
+
+		let res_copy = &inc - &dec;
+		let res_move = inc - dec;
+		assert_eq!(res_copy, res_move);
+
+		for n in res_move {
+			assert_eq!(n, 0);
+		}
+	}
+	
+	// Test != rows
+	{
+		let small = Matrix::new(3, 3, 0..9);
+		let large = Matrix::new(6, 3, 0..18);
+
+		let res = catch_unwind(|| small - large);
+		assert!(res.is_err());
+	}
+
+	// Test != colums
+	{
+		let small = Matrix::new(3, 3, 0..9);
+		let large = Matrix::new(3, 6, 0..18);
+
+		let res = catch_unwind(|| small - large);
 		assert!(res.is_err());
 	}
 }
