8293fb376065753835bf3af456a43777446aee93
Nicolas Mémeint <nicolas.memeint@gmail.com>
2018-08-28 11:38:05 +0200

    Randomized values of matrices in benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,8 +1,29 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use matrix_rs::Matrix;
+use rand::distributions::{Distribution, Standard};
+use rand::prelude::random;
+
+fn randomize<T>(m: &mut Matrix<T>)
+where
+    Standard: Distribution<T>,
+{
+    m.apply_mut(|v| *v = random());
+}
+
+fn matrix_small() -> Matrix<i32> {
+    let mut m = Matrix::new(3, 3);
+    randomize(&mut m);
+    m
+}
+
+fn matrix_large() -> Matrix<i32> {
+    let mut m = Matrix::new(100, 100);
+    randomize(&mut m);
+    m
+}
 
 fn bench_get_row_small(c: &mut Criterion) {
-    let m: Matrix<i32> = Matrix::new(3, 3);
+    let m = matrix_small();
     c.bench_function("get_row_small", move |b| {
         b.iter(|| {
             for r in 0..m.rows() {
@@ -13,7 +34,7 @@ fn bench_get_row_small(c: &mut Criterion) {
 }
 
 fn bench_get_row_large(c: &mut Criterion) {
-    let m: Matrix<i32> = Matrix::new(100, 100);
+    let m = matrix_large();
     c.bench_function("get_row_large", move |b| {
         b.iter(|| {
             for r in 0..m.rows() {
@@ -24,7 +45,7 @@ fn bench_get_row_large(c: &mut Criterion) {
 }
 
 fn bench_get_col_small(c: &mut Criterion) {
-    let m: Matrix<i32> = Matrix::new(3, 3);
+    let m = matrix_small();
     c.bench_function("get_col_small", move |b| {
         b.iter(|| {
             for c in 0..m.cols() {
@@ -35,7 +56,7 @@ fn bench_get_col_small(c: &mut Criterion) {
 }
 
 fn bench_get_col_large(c: &mut Criterion) {
-    let m: Matrix<i32> = Matrix::new(100, 100);
+    let m = matrix_large();
     c.bench_function("get_col_large", move |b| {
         b.iter(|| {
             for c in 0..m.cols() {
